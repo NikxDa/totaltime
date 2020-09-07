7581b085637dbbd4d9cadbb1adab08914447a523
NikxDa <NikxDa@users.noreply.github.com>
2020-09-07 04:22:57 +0200

    Add CLI structure

---
+++ lib/cli.rb
@@ -0,0 +1,47 @@
+require "optparse"
+require "tempfile"
+require "readline"
+require "colorize"
+
+require "duration.rb"
+
+class CLI
+    def self.run
+        OptionParser.new do |parser| 
+            parser.banner = "Usage: timesheet [options]"
+
+            parser.on("-h", "--help", "Show this help message") do ||
+                puts parser
+                exit
+            end
+        end.parse!
+
+        puts "Press [Enter] on an empty line to confirm".light_black
+        totalDuration = Duration.new
+
+        lineUp = "\033[1A"
+        lineClear = "\033[K"
+
+        prompt = "> ".light_black
+        promptOverride = lineUp + "\r" + lineClear + prompt
+
+        while line = Readline.readline(prompt, true)
+            if line.empty?
+                print lineUp
+                break
+            end
+
+            duration = Duration.try_convert(line)
+            
+            if duration != nil
+                totalDuration += duration
+                puts promptOverride + duration.to_s.green
+            elsif
+                invalidText = (line + " [Invalid]").red
+                puts promptOverride + invalidText
+            end
+        end
+
+        puts "Total time: ".light_black + totalDuration.to_s.cyan
+    end
+end
