4e36817befd146b57df5d4784baae60cef4a65f9
NikxDa <NikxDa@users.noreply.github.com>
2020-09-07 03:28:54 +0200

    Add test cases

---
+++ test/tc_duration.rb
@@ -0,0 +1,106 @@
+require "../lib/duration.rb"
+
+class TestSimpleNumber < Test::Unit::TestCase
+    def test_creation
+        dur = Duration.new(1,1)
+
+        assert_instance_of(Duration, dur)
+    end
+
+    def test_addition
+        dur1 = Duration.new(1,2)
+        dur2 = Duration.new(3,4)
+        sum = dur1 + dur2
+
+        assert_instance_of(Duration, sum)
+        assert_equal(sum.hours, 4)
+        assert_equal(sum.minutes, 6)
+    end
+
+    def test_wrapping
+        dur1 = Duration.new(1,30)
+        dur2 = Duration.new(1,31)
+        sum = dur1 + dur2
+
+        assert_instance_of(Duration, sum)
+        assert_equal(sum.hours, 3)
+        assert_equal(sum.minutes, 1)
+    end
+
+    def test_string_parse
+        parseStr = "1:35"
+        dur = Duration.try_convert(parseStr)
+
+        assert_not_nil(dur)
+        assert_instance_of(Duration, dur)
+        assert_equal(dur.hours, 1)
+        assert_equal(dur.minutes, 35)
+    end
+
+    def test_colon_parse
+        parseStr = ":45"
+        dur = Duration.try_convert(parseStr)
+
+        assert_not_nil(dur)
+        assert_instance_of(Duration, dur)
+        assert_equal(dur.hours, 0)
+        assert_equal(dur.minutes, 45)
+
+        parseStr = ":90"
+        dur = Duration.try_convert(parseStr)
+
+        assert_not_nil(dur)
+        assert_instance_of(Duration, dur)
+        assert_equal(dur.hours, 1)
+        assert_equal(dur.minutes, 30)
+    end
+
+    def test_float_parse
+        parseStr = "1.5"
+        dur = Duration.try_convert(parseStr)
+
+        assert_not_nil(dur)
+        assert_instance_of(Duration, dur)
+        assert_equal(dur.hours, 1)
+        assert_equal(dur.minutes, 30)
+
+        parseStr = ".5"
+        dur = Duration.try_convert(parseStr)
+
+        assert_not_nil(dur)
+        assert_instance_of(Duration, dur)
+        assert_equal(dur.hours, 0)
+        assert_equal(dur.minutes, 30)
+
+        parseStr = "3.33333333333"
+        dur = Duration.try_convert(parseStr)
+
+        assert_not_nil(dur)
+        assert_instance_of(Duration, dur)
+        assert_equal(dur.hours, 3)
+        assert_equal(dur.minutes, 20)
+    end
+
+    def test_invalid_parse
+        parseStr = "1.5."
+        dur = Duration.try_convert(parseStr)
+
+        assert_nil(dur)
+
+        parseStr = "invalid"
+        dur = Duration.try_convert(parseStr)
+
+        assert_nil(dur)
+
+        parseStr = ":"
+        dur = Duration.try_convert(parseStr)
+
+        assert_nil(dur)
+    end
+
+    def test_typecheck
+        assert_raise(TypeError) {
+            Duration.new("a", 2)
+        }
+    end
+end
---
+++ test/ts_all.rb
@@ -0,0 +1,2 @@
+require "test/unit"
+require "./tc_duration.rb"
